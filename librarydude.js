66392ae02703b2d1ad1040f9dd0e5e9c91cf1069
Prathamx <73588788+Prathamx@users.noreply.github.com>
2020-11-08 13:27:57 +0530

    Add files via upload

---
+++ librarydude.js
@@ -0,0 +1,10 @@
+function colided(bullet1,wall1) {
+    bulletRightEdge = bullet1.x +bullet1.width;
+    wallLeftEdge = wall1.x;
+    if(bulletRightEdge >= wallLeftEdge) {
+    return true;
+    }
+    else {
+      return false;  
+    }
+}
